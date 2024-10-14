d1e6d3a1df20b2c9ebf48286264774eb30321e46
ist1102500 <alice.arnaud.mota@tecnico.ulisboa.pt>
2024-10-14 22:57:06 +0100

    working parser and model

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import sys
 import minizinc
 import re
@@ -15,97 +16,6 @@ initial_info = []
 min_time = 0
 max_time = 0
 
-def read_input_file(input_file):
-    """Reads the input data from the file and parses it into a suitable format."""
-    with open(input_file, 'r') as file:
-        data = file.readlines()
-        #Parse the input TODO
-
-    print(int(data[0].split(':')[-1]))
-    parse_input_file(data)
-    return data
-
-
-def parse_input_file(data):
-    """Parses the input data into a suitable format."""
-
-    global num_machines
-    global num_resources
-    global num_tests
-    
-    # initial_info = [] #will store number of tests [0], number of machines [1], number of resources [2], makespan [3]
-    for line in data:
-        #print(line)
-        if line.startswith('%'):
-            match = re.search(r'\d+', line)
-            initial_info.append(int(match.group()))
-            
-
-        else:
-            # print(initial_info)
-            num_machines = initial_info[1]
-            parse_test_data(line)  #parse test data
-            
-    num_tests = initial_info[0]
-    num_resources = initial_info[2]
-
-    process_tests()
-
-    return
-
-def parse_test_data(line):
-    # Regular expressions to capture test data - going to be used ahead
-    test_pattern = re.compile(r"test\(\s*'(\w+)',\s*(\d+),\s*\[(.*?)\],\s*\[(.*?)\]\)")
-
-    match = test_pattern.match(line)    # Match test data
-
-    if match:
-        test_id = match.group(1)
-        duration = int(match.group(2))
-        # Convert machine list to integers, stripping any extraneous characters
-        machines = set(int(m.strip().strip("'\"")[1:]) for m in match.group(3).split(',') if m)
-        # Convert resource list to integers, stripping any extraneous characters
-        resources = set(int(r.strip().strip("'\"")[1:]) for r in match.group(4).split(',') if r)
-        # print(test_id)
-        
-    # Append test data as dictionary
-        tests.append({
-            'test_id': test_id,
-            'duration': duration,
-            'machines': machines,
-            'resources': resources
-        })
-
-    for i in range(1, num_machines + 1):
-        all_machines.add(i)
-    
-    return tests
-
-def process_tests():
-    tests.sort(key=lambda x: x['resources'], reverse=True)
-    tests.sort(key=lambda x: x['machines'], reverse=True)
-    
-    # tests.sort(key=lambda x: x['resources'], reverse=True)
-    # tests.sort(key=lambda x: x['duration'])
-    for test in tests:
-        duration = test['duration']
-        machines = set(test['machines'])
-        resources = set(test['resources'])
-
-        durations.append(duration)
-
-        if machines == set(): 
-            available_machines.append(all_machines)
-        else: 
-            available_machines.append(machines)
-        
-        required_resources.append(resources)
-
-    global max_time 
-    max_time = sum(durations)
-    global min_time 
-    min_time = max(durations)
-
 def check_command_line_arguments():
     print(sys.argv)
     if len(sys.argv) != 3:
@@ -121,7 +31,94 @@ def check_command_line_arguments():
         sys.exit(1)
     
     else: print ("Input and output files are valid.\n")
+
+def read_input_file(input_file):
+    """Reads the input data from the file and parses it into a suitable format."""
+    with open(input_file, 'r') as file:
+        data = file.readlines()
+
+    parse_input_file(data)
+    return data
+
+def parse_input_file(data):
+    """Parses the input data into a suitable format."""
+
+    global num_machines
+    global num_resources
+    global num_tests
     
+    for line in data:
+        if line.startswith('%'):
+            match = re.search(r'\d+', line)
+            initial_info.append(int(match.group()))
+
+        else:
+            num_machines = initial_info[1]
+            parse_test_data(line)
+            
+    num_tests = initial_info[0]
+    num_resources = initial_info[2]
+
+    process_tests()
+    return
+
+def parse_test_data(line):
+    # Regular expressions to capture test data
+    test_pattern = re.compile(r"test\(\s*'(\w+)',\s*(\d+),\s*\[(.*?)\],\s*\[(.*?)\]\)")
+
+    match = test_pattern.match(line)    # Match test data
+
+    if match:
+        test_id = match.group(1)
+        duration = int(match.group(2))
+        # Convert machine list to integers
+        machines = set(int(m.strip().strip("'\"")[1:]) for m in match.group(3).split(',') if m)
+        # Convert resource list to integers
+        resources = set(int(r.strip().strip("'\"")[1:]) for r in match.group(4).split(',') if r)
+        
+    # Append test data as dictionary
+        tests.append({
+            'test_id': test_id,
+            'duration': duration,
+            'machines': machines,
+            'resources': resources
+        })
+
+    for i in range(1, num_machines + 1):
+        all_machines.add(i)
+    
+    return tests
+
+def calculate_min_time():
+    # Calculate min_time as the maximum sum of durations of tests that use the same resources
+    resource_groups = {}
+    for i, resource_set in enumerate(required_resources):
+        for resource in resource_set:
+            if resource not in resource_groups:
+                resource_groups[resource] = 0
+            resource_groups[resource] += durations[i]
+
+    return max(resource_groups.values())
+
+def process_tests():
+    # Extract test data into separate lists
+    for test in tests:
+        duration = test['duration']
+        machines = set(test['machines'])
+        resources = set(test['resources'])
+
+        durations.append(duration)
+
+        if machines == set(): 
+            available_machines.append(all_machines)
+        else: 
+            available_machines.append(machines)
+        
+        required_resources.append(resources)
+
+    global max_time, min_time 
+    max_time = sum(durations)
+    min_time = calculate_min_time()
 
 def solve_problem():
     model = minizinc.Model("model.mzn")
@@ -131,28 +128,51 @@ def solve_problem():
     instance = minizinc.Instance(solver, model)
 
     instance["num_tests"] = num_tests
-    instance["num_machines"] = num_machines
-    instance["num_resources"] = num_resources
     instance["max_time"] = max_time
     instance["min_time"] = min_time
+    instance["num_machines"] = num_machines
+    instance["num_resources"] = num_resources
     instance["duration"] = durations
     instance["required_resources"] = required_resources
     instance["available_machines"] = available_machines
 
-    # print(f"max_time: {max_time}")
-    # print(f"min_time: {min_time}")
-    # print(f"num_tests: {num_tests}")
-    # print(f"num_machines: {num_machines}")
-    # print(f"num_resources: {num_resources}")
-    # print(f"duration: {durations}")
-    # print(f"required_resources: {required_resources}")
-    # print(f"available_machines: {available_machines}")
+    result = instance.solve(free_search=True, timeout=timedelta(seconds=290))
+    return result
 
+def get_output(result):
+    makespan_obtained = result["makespan"]
+    print(f"Obtained makespan: {makespan_obtained}")
+    starts = result["start_times"]
+    print(f"Start times: {starts}")
+    machines_selected = result["selected_machine"]
+    print(f"Machines selected: {machines_selected}")
+    return format_output(makespan_obtained, starts, machines_selected)
 
-    result = instance.solve()
-    print(f"Solution: {result}")
+def format_output(makespan, start_times, selected_machine):
+    machine_assignments = {}
 
+    # Group tests by machine
+    for i, machine in enumerate(selected_machine):
+        test_id = f"t{i+1}"
+        start_time = start_times[i]
+        # res = required_resources[i] if required_resources[i] != set() else []
+        
+        # # If no resources provided, assign empty list
+        res = required_resources[i] if required_resources and i < len(required_resources) else []
+        
+        if machine not in machine_assignments:
+            machine_assignments[machine] = []
+        machine_assignments[machine].append((test_id, start_time, res))
 
+    # Format the output string
+    output_str = f"% Makespan : {makespan}\n"
+    for machine, tests in sorted(machine_assignments.items()):
+        formatted_tests = ", ".join([f"(\'{test[0]}\',{test[1]}" + (f",[{','.join([f'\'r{r}\'' for r in test[2]])}]" if test[2] else "") + ")" for test in tests])
+        output_str += f"machine( \'m{machine}\', {len(tests)}, [{formatted_tests}])\n"
+
+    print(output_str)
+
+    return output_str  
 
 def main():
     start_time = time.time()
@@ -162,15 +182,14 @@ def main():
 
     output_file = sys.argv[2]
     
-    solution = solve_problem()
+    result = solve_problem()
 
     with open(output_file, 'w') as file:
-        file.write("Solution " + str(solution) + ";\n")
-
+        file.write(str(get_output(result)) + "\n")
+        
     end_time = time.time()  # Record the end time
     elapsed_time = end_time - start_time  # Calculate the elapsed time
-    print(f"Elapsed time: {elapsed_time:.2f} seconds")  # Print the elapsed time
-
+    print(f"Elapsed time: {elapsed_time:.2f} seconds") 
 
 if __name__ == "__main__":
     main()
